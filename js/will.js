f598edc8ae20398528a96f8d5ca4a03b06b7f736
LikeJasper <williamclark.home@gmail.com>
2015-10-12 18:19:41 +0200

    Basic color scheme and layout in place with core effects

---
+++ js/will.js
@@ -0,0 +1,27 @@
+/*global
+  window
+*/
+
+(function () {
+  'use strict';
+
+  $(document).ready(function () {
+    $('nav, nav a').hover(function () {
+      $(this).find('.slide-in').toggleClass('hidden');
+    }, function () {
+      $(this).find('.slide-in').toggleClass('hidden');
+    });
+
+    $('nav a').click(function (e) {
+      e.preventDefault();
+      var target = this.hash,
+        $target = $(target);
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 1200, 'easeInOutCubic', function () {
+        window.location.hash = target;
+      });
+    });
+  });
+
+}());
